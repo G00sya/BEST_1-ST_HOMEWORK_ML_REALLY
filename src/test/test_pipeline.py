41c8ef9078fc4b74bdaf745183b994f022c2b43d
Ishchenko Igor <igoryan11060@gmail.com>
2025-06-04 17:19:39 +0300

    feat: Add test_pipeline file

---
+++ src/test/test_pipeline.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.fixture
+def train_dataset():
+    # note: реализуйте и протестируйте подготовку данных (скачиание и препроцессинг)
+    pass
+
+
+@pytest.mark.parametrize(["device"], [["cpu"], ["cuda"]])
+def test_train_on_one_batch(device, train_dataset):
+    # note: реализуйте и протестируйте один шаг обучения вместе с метрикой
+    pass
+
+
+def test_training():
+    # note: реализуйте и протестируйте полный цикл обучения модели (обучение, валидацию, логирование, сохранение артефактов)
+    pass
